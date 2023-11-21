599fce44a00ed1e651270ebf36b88b97d0a049c1
Ryan Luis <RyanCLuis@gmail.com>
2023-11-20 22:04:35 -0800

    Players win and can click on squares

--- main.js/main.js
+++ main.js/main.js
@@ -2,6 +2,7 @@ let playerText = document.getElementById('playerText')
 let restartBtn = document.getElementById('restartBtn')
 let boxes = [...document.getElementsByClassName('box')]
 
+
 const oText = "O"
 const xText = "X"
 let currentPlayer = xText
@@ -9,6 +10,25 @@ let spaces = Array(9).fill(null)
 
 const startGame = () => {
     boxes.forEach(box => box.addEventListener('click', boxClicked))
+}
+
+function boxClicked(click) {
+    const id = click.target.id
+
+    if(!spaces[id]){
+        spaces[id] = currentPlayer
+        click.target.innerText = currentPlayer
+
+        if(playerHasWon() !==false){
+            playerText.innerHTML = `${currentPlayer} is amazing and won!`
+            let winningBlocks = playerHasWon()
+
+            winningBlocks.map( box => boxes[box].style.backgroundColor=winnerIndicator)
+            return
+        }
+
+        currentPlayer = currentPlayer == xText ? oText : xText
+    }
 }
 
 const winningCombos = [
@@ -21,3 +41,31 @@ const winningCombos = [
     [0,4,8],
     [2,4,6]
 ]
+
+function playerHasWon() {
+    for (const condition of winningCombos) {
+        let [a, b, c] = condition
+
+        if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
+            return [a,b,c]
+        }
+    }
+    return false
+}
+
+restartBtn.addEventListener('click', restart)
+
+function restart() {
+    spaces.fill(null)
+
+    boxes.forEach( box => {
+        box.innerText = ''
+        box.style.backgroundColor=''
+    })
+
+    playerText.innerHTML = 'Tic Tac Toe'
+
+    currentPlayer = xText
+}
+
+startGame()
